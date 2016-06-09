7d1db303008aa5a29c8a687e406f107fd72cb87c
Tobias Andersen <j.tobiasandersen@gmail.com>
2016-06-09 12:50:17 +0200

    Add hist alias to git

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,4 +18,4 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
-alias hist = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+alias ghist = 'git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
