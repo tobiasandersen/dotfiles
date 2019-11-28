1cdead4573a07e8e13b4dde5136f18999a907d3e
Tobias Andersen <j.tobiasandersen@gmail.com>
2019-11-28 18:39:11 +0100

    fix aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,4 +18,5 @@ alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
-alias ghist = 'git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+alias ghist='git log --pretty=format:"%h %ad | %s%d [%an]" --graph --date=short'
+alias gfixup='!f() { TARGET=$(git rev-parse "$1"); shift; git commit --fixup=$TARGET $@ && EDITOR=true git rebase -i --autostash --autosquash $TARGET^; }; f'
